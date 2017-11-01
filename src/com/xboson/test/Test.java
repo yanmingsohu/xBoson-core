6102f1f7e5e347bc4c9c2e41e3b731ff12e82a58
yanming <yanming-sohu@sohu.com>
2017-11-01 11:10:02 +0800

    Signed-off-by: yanming <yanming-sohu@sohu.com>

---
+++ src/com/xboson/test/Test.java
@@ -0,0 +1,12 @@
+/* CatfoOD 2017年11月1日 上午11:08:21 yanming-sohu@sohu.com Q.412475540 */
+
+package com.xboson.test;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello xBoson");
+	}
+
+}
