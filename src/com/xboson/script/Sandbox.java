6cfa54d30a0b59a287bb0fa01982056285b26e43
yanming <yanming-sohu@sohu.com>
2017-12-12 17:05:43 +0800

    格式化.
    Signed-off-by: yanming <yanming-sohu@sohu.com>

--- src/com/xboson/script/Sandbox.java
+++ src/com/xboson/script/Sandbox.java
@@ -37,59 +37,59 @@ import sun.font.Script;
  * 一个编译好的脚本, 可以反复运行.
  */
 public class Sandbox {
-	
-	private ScriptEngine js;
-	private Bindings bind;
-	private ScriptContext context;
-	private boolean ispred = false;
-	private Compilable cpl;
 
-	
-	Sandbox(ScriptEngine engine) throws ScriptException {
-		this.js 			= engine;
-		this.bind 		= js.getBindings(ScriptContext.ENGINE_SCOPE);
-		this.context 	= js.getContext();
-		
-		if (js instanceof Compilable) {
-			cpl = (Compilable) js;
-		} else {
-			throw new ScriptException("cannot compile");
-		}
-	}
-	
-	
-	/**
-	 * 冻结对象
-	 * @param name - 如果为空则冻结全局对象
-	 * @throws ScriptException
-	 */
-	public void freeze(String name) throws ScriptException {
-		if (name == null) {
-			throw new ScriptException("parm 'name' is null");
-		}
-		eval("Object.freeze(" + name + ")");
-	}
-	
-	
-	/**
-	 * 冻结全局对象, 即使 hack 也无法修改全局绑定的工具对象,
-	 * 无法在全局定义变量/函数.
-	 * 
-	 * !!锁住导致文件名无法与脚本绑定, 抛出的异常没有文件路径
-	 * 
-	 * @throws ScriptException
-	 */
-	public void freezeGlobal() throws ScriptException {
-		freeze("this");
-	}
-	
-	
-	/**
-	 * 将代码包装到函数中, 并且使用 nodejs 语法
-	 */
-	public WarpdScript warp(String filename, String code) throws ScriptException {
-		return new WarpdScript(this, code, filename);
-	}
+  private ScriptEngine js;
+  private Bindings bind;
+  private ScriptContext context;
+  private boolean ispred = false;
+  private Compilable cpl;
+
+
+  Sandbox(ScriptEngine engine) throws ScriptException {
+    this.js 			= engine;
+    this.bind 		= js.getBindings(ScriptContext.ENGINE_SCOPE);
+    this.context 	= js.getContext();
+
+    if (js instanceof Compilable) {
+      cpl = (Compilable) js;
+    } else {
+      throw new ScriptException("cannot compile");
+    }
+  }
+
+
+  /**
+   * 冻结对象
+   * @param name - 如果为空则冻结全局对象
+   * @throws ScriptException
+   */
+  public void freeze(String name) throws ScriptException {
+    if (name == null) {
+      throw new ScriptException("parm 'name' is null");
+    }
+    eval("Object.freeze(" + name + ")");
+  }
+
+
+  /**
+   * 冻结全局对象, 即使 hack 也无法修改全局绑定的工具对象,
+   * 无法在全局定义变量/函数.
+   *
+   * !!锁住导致文件名无法与脚本绑定, 抛出的异常没有文件路径
+   *
+   * @throws ScriptException
+   */
+  public void freezeGlobal() throws ScriptException {
+    freeze("this");
+  }
+
+
+  /**
+   * 将代码包装到函数中, 并且使用 nodejs 语法
+   */
+  public WarpdScript warp(String filename, String code) throws ScriptException {
+    return new WarpdScript(this, code, filename);
+  }
 
 
   /**
@@ -98,44 +98,48 @@ public class Sandbox {
   public WarpdScript warp(String code) throws ScriptException {
     return new WarpdScript(this, code, "<warp>");
   }
-	
-	
-	public Object eval(String code) throws ScriptException {
-		// System.out.println("CODE: " + code);
-		return js.eval(code);
-	}
-	
-	
-	public Object eval(InputStream instream) throws ScriptException {
-		try {
-			Reader reader = new InputStreamReader(instream);
-			return js.eval(reader);
-		} finally {
-			Tool.close(instream);
-		}
-	}
-	
-	
-	public CompiledScript compile(String code) throws ScriptException {
-		return cpl.compile(code);
-	}
 
 
-	public Invocable getGlobalFunc() {
-		return (Invocable) js;
-	}
+  public Object eval(String code) throws ScriptException {
+    // System.out.println("CODE: " + code);
+    return js.eval(code);
+  }
 
 
-	public Object bootstrap() throws ScriptException {
-		if (ispred) return null;
-		ispred = true;
-		setFilename("<bootstrap>");
-		InputStream script1 = getClass().getResourceAsStream("./bootstrap.js");
-		return eval(script1);
-	}
+  public Object eval(InputStream instream) throws ScriptException {
+    try {
+      Reader reader = new InputStreamReader(instream);
+      return js.eval(reader);
+    } finally {
+      Tool.close(instream);
+    }
+  }
 
 
-	public void bootstrapEnvReady() throws ScriptException {
+  public CompiledScript compile(String code) throws ScriptException {
+    try {
+      return cpl.compile(code);
+    } catch(ScriptException e) {
+      throw new JScriptException(e);
+    }
+  }
+
+
+  public Invocable getGlobalFunc() {
+    return (Invocable) js;
+  }
+
+
+  public Object bootstrap() throws ScriptException {
+    if (ispred) return null;
+    ispred = true;
+    setFilename("<bootstrap>");
+    InputStream script1 = getClass().getResourceAsStream("./bootstrap.js");
+    return eval(script1);
+  }
+
+
+  public void bootstrapEnvReady() throws ScriptException {
     try {
       getGlobalFunc().invokeFunction("__env_ready");
     } catch(NoSuchMethodException e) {
@@ -144,29 +148,29 @@ public class Sandbox {
   }
 
 
-	public void bootstrapEnd() throws ScriptException {
-		try {
-			getGlobalFunc().invokeFunction("__boot_over");
+  public void bootstrapEnd() throws ScriptException {
+    try {
+      getGlobalFunc().invokeFunction("__boot_over");
     } catch(NoSuchMethodException e) {
-			throw new ScriptException(e);
-		}
-	}
-	
-	
-	/**
-	 * 在 compile 之前设置有效
-	 */
-	public void setFilename(String name) {
-		context.setAttribute(ScriptEngine.FILENAME, name, ScriptContext.ENGINE_SCOPE);
-	}
-	
-	
-	public Bindings getBindings() {
-		return bind;
-	}
-	
-	
-	public ScriptContext getContext() {
-		return context;
-	}
+      throw new ScriptException(e);
+    }
+  }
+
+
+  /**
+   * 在 compile 之前设置有效
+   */
+  public void setFilename(String name) {
+    context.setAttribute(ScriptEngine.FILENAME, name, ScriptContext.ENGINE_SCOPE);
+  }
+
+
+  public Bindings getBindings() {
+    return bind;
+  }
+
+
+  public ScriptContext getContext() {
+    return context;
+  }
 }
